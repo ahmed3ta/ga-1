f5aacf1e1896ce19bd9cf5448aba9c52c966e8d5
Ahmed Atta <ahmed.x.atta@pwc.com>
2025-07-26 12:23:49 +0300

    script2

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,3 +1,5 @@
-ls -ltrah
-cat README.md
-sleep 15
+sudo apt-get update -y
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+grep -i "dragon" dragon.txt
+ls -ltra
